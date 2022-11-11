64c809929ec70060a7b6ba4ba8f87ed5ae0e7719
rubymaxioned <axioned0482022>
2022-11-11 18:17:23 +0530

    final

---
+++ script.js
@@ -0,0 +1,11 @@
+var li = document.querySelectorAll(".gallery ul li");
+console.log(li);
+
+li.forEach(function(list){
+    list.addEventListener("click",function(){
+        for(var i=0; i<li.length; i++){
+            li[i].style.flexBasis = "20%";
+        }
+        list.style.flex = "1 1 100%";
+    });
+})
