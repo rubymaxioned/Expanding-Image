076f8ee41f79c5f18308175b0acd6ec5bbedd5e4
rubymaxioned <axioned0482022>
2022-11-21 12:28:19 +0530

    added span

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 var li = document.querySelectorAll(".gallery ul li");
-console.log(li);
+var span = document.querySelectorAll("span");
+console.log(span);
 
 li.forEach(function (list) {
     list.addEventListener("click", function () {
         for (var i = 0; i < li.length; i++) {
             li[i].style.flexBasis = "20%";
         }
-        list.style.flex = "1 1 100%";
+        list.style.flex = "1 1 100%"; 
     });
 })
