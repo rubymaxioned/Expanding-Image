4caf225ce30f4abd773765a3968d83d4bdffb088
rubymaxioned <axioned0482022>
2022-11-28 19:03:13 +0530

    added figure caption

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,13 @@ var li = document.querySelectorAll(".gallery ul li");
 var span = document.querySelectorAll("span");
 console.log(span);
 
-li.forEach(function (list) {
+li.forEach(function (list,index) {
     list.addEventListener("click", function () {
         for (var i = 0; i < li.length; i++) {
             li[i].style.flexBasis = "20%";
+            span[i].classList.remove('show');
         }
         list.style.flex = "1 1 100%"; 
+        span[index].classList.add('show');
     });
 })
